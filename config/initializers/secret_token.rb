dd00881bf650ed71760f935f0d8a38cec313b5da
Daniel Oliveira <danieloliveira.lei@gmail.com>
2013-04-26 23:39:44 +0100

    teste

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spin::Application.config.secret_token = '16db156c4a0e18faea1db7c675320e64fc1ce3533ada1741f040e5c0af28b279852f89dda90612c3df5578cc713e5ef908a6f3a337f02a27b2f0012f33e71a42'
